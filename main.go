0cba401e66b6656c3f88941c4ee8003db5cdf3cc
EyadehAlzoubi <eyadahalzoubi029@gmail.com>
2026-05-31 18:14:59 +0300

    Add POST /events endpoint and update getEvents function to retrieve events from models

--- main.go
+++ main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	server :=gin.Default()
+	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvents)
+
 	server.Run(":8080")
 }
 
+func getEvents(context *gin.Context) {
+	events := models.GetAll()
+	context.JSON(http.StatusOK, events)
+}
 
-func getEvents(context *gin.Context){
-	context.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+func createEvents(context *gin.Context) {
+	var newEvent models.Event
+	err := context.ShouldBindJSON(&newEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+		return
+	}
+
+	newEvent.ID = 1
+	newEvent.UserID = 1
+
+	newEvent.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": newEvent})
+
 }
