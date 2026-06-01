718f84770edd50ad3b103173f7a2770d3f2302b8
EyadehAlzoubi <eyadahalzoubi029@gmail.com>
2026-06-02 00:01:55 +0300

    Fix formatting in users table creation SQL statement in db.go by removing unnecessary trailing whitespace.

--- db/db.go
+++ db/db.go
@@ -30,7 +30,7 @@ func createTables() {
 	CREATE TABLE IF NOT EXISTS users (
 	    id INTEGER PRIMARY KEY AUTOINCREMENT,
 		email TEXT NOT NULL UNIQUE,
-		password TEXT NOT NULL,
+		password TEXT NOT NULL
 	)
 	`
 
