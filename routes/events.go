bcdce944821cb9df29f33a44d200398b2391c647
EyadehAlzoubi <eyadahalzoubi029@gmail.com>
2026-06-01 22:23:44 +0300

    Add event handling routes in events.go for CRUD operations including getEvents, getEvent, createEvents, and updateEvent functions

---
+++ routes/events.go
@@ -0,0 +1,95 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAll()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"ERROR1": err.Error()})
+		return
+
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"ERROR_getEvent": err.Error()})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"ERROR_getEvent": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+
+}
+
+func createEvents(context *gin.Context) {
+	var newEvent models.Event
+	err := context.ShouldBindJSON(&newEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+		return
+	}
+
+	newEvent.ID = 1
+	newEvent.UserID = 1
+
+	err = newEvent.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"ERROR2": err.Error()})
+		return
+
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": newEvent})
+
+}
+
+func updateEvent(context *gin.Context) {
+
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"updateEvent": err.Error()})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"updateEvent": err.Error()})
+		return
+	}
+
+	var updateEvent models.Event
+
+	err = context.ShouldBindJSON(&updateEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+		return
+	}
+
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"ERROR": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "event Updated successfully"})
+
+}
